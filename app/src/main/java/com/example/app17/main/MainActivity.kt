ec09cae3afff087682805e7fd604672c211963fe
Carlos-205 <capp.4321@gmail.com>
2026-03-20 23:04:55 -0500

    Commit para la edición de barra de notificaciones y menu desplegable hacia la derecha

--- app/src/main/java/com/example/app17/main/MainActivity.kt
+++ app/src/main/java/com/example/app17/main/MainActivity.kt
@@ -2,16 +2,46 @@ package com.example.app17.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.app17.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById <Toolbar> (R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+
+        setSupportActionBar(toolbar)
+
+        val toogle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        toogle.drawerArrowDrawable.color = ContextCompat.getColor(this, R.color.black)
     }
 }
