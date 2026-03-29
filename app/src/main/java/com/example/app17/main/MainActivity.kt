54643d01a7a420b09a4669c50c75bbac4dae2baf
Carlos-205 <capp.4321@gmail.com>
2026-03-28 23:59:40 -0500

    Coomit para el funcionamiento de los fragments y sus respectivos items en menu_bottom_nav.xml, además de su funcionalidad en el MainActivity.kt

--- app/src/main/java/com/example/app17/main/MainActivity.kt
+++ app/src/main/java/com/example/app17/main/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.app17.R
+import com.example.app17.main.perfil.perfil_fragment
+import com.example.app17.main.productos.carrito_fragment
+import com.example.app17.main.productos.catalogo_fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.example.app17.main.productos.home_fragment
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,5 +48,23 @@ class MainActivity : AppCompatActivity() {
         toogle.syncState()
 
         toogle.drawerArrowDrawable.color = ContextCompat.getColor(this, R.color.black)
+
+        cargarFragment(home_fragment())
+        bottomNav.selectedItemId = R.id.nav_home
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> cargarFragment(home_fragment())
+                R.id.nav_catalogo -> cargarFragment(catalogo_fragment())
+                R.id.nav_carrito -> cargarFragment(carrito_fragment())
+                R.id.nav_perfil -> cargarFragment(perfil_fragment())
+            }
+            true
+        }
+    }
+    private fun cargarFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
