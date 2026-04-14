672ae007e8cd6973f2ddbdf4e65280dfef56f9eb
Carlos-205 <capp.4321@gmail.com>
2026-04-13 23:43:03 -0500

    Commit de la validación en login con los datos de supabase

--- app/src/main/java/com/example/app17/auth/LoginActivity.kt
+++ app/src/main/java/com/example/app17/auth/LoginActivity.kt
@@ -3,18 +3,95 @@ package com.example.app17.auth
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.app17.R
+import com.example.app17.SupabaseClient
 import com.example.app17.main.MainActivity
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+import kotlinx.coroutines.launch
+import android.widget.ScrollView
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etCorreo: EditText
+    private lateinit var etContrasena: EditText
+    private lateinit var btnIngresar: Button
+    private lateinit var tvRegistro: TextView
+    private lateinit var btnGoogle: Button
+    private lateinit var tvRecuperarContrasena: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        //Configuración de rootview de teclado
+        val rootView = findViewById<LinearLayout>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+
+            val bottomPadding = maxOf(systemBars.bottom, imeInsets.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                bottomPadding
+            )
+            insets
+        }
+
+        etCorreo = findViewById(R.id.txt_incorreo)
+        etContrasena = findViewById(R.id.txt_inpassword)
+        btnIngresar = findViewById(R.id.button_ingresar)
+        tvRecuperarContrasena = findViewById(R.id.txt_recuperarcontra)
+        btnGoogle = findViewById(R.id.button_google)
+        tvRegistro = findViewById(R.id.txt_register)
+
+        //Inicio de sesion con informacion de SupaBase
+        btnIngresar.setOnClickListener {
+            val correo = etCorreo.text.toString().trim()
+            val contrasena = etContrasena.text.toString().trim()
+
+            //Validaciones de campos
+            if (correo.isEmpty() || contrasena.isEmpty()) {
+                Toast.makeText(this, "Por favor complete la información", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (contrasena.length < 8) {
+                Toast.makeText(this, "La contraseña debe tener al menos 8 caracteres", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //Validacion - Consulta en supabase
+            lifecycleScope.launch {
+                try {
+                    SupabaseClient.client.auth.signInWith(Email) {
+                        email = correo
+                        password = contrasena
+                    }
+                    runOnUiThread {
+                        Toast.makeText(this@LoginActivity, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        finish()
+                    }
+                } catch (e: Exception) {
+                    runOnUiThread{
+                        Toast.makeText(this@LoginActivity, "Error al iniciar sesión: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
         findViewById<TextView>(R.id.txt_register).setOnClickListener {
             startActivity(Intent(this, RegistroActivity::class.java))
