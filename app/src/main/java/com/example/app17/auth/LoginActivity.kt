1de11c8bc58654ce12a81bb797ab736e99264dea
Carlos-205 <capp.4321@gmail.com>
2026-04-14 14:17:52 -0500

    Commit para la creación de la funcionalidad de iniciar con google

--- app/src/main/java/com/example/app17/auth/LoginActivity.kt
+++ app/src/main/java/com/example/app17/auth/LoginActivity.kt
@@ -19,6 +19,13 @@ import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.providers.builtin.Email
 import kotlinx.coroutines.launch
 import android.widget.ScrollView
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import io.github.jan.supabase.auth.providers.Google
+import io.github.jan.supabase.auth.providers.builtin.IDToken
+import java.nio.channels.spi.AsynchronousChannelProvider.provider
 
 class LoginActivity : AppCompatActivity() {
 
@@ -93,12 +100,49 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        //Inicio de sesion con informacion de Google
+        btnGoogle.setOnClickListener {
+            iniciarSesionConGoogle()
+
+        }
+
         findViewById<TextView>(R.id.txt_register).setOnClickListener {
             startActivity(Intent(this, RegistroActivity::class.java))
         }
 
         findViewById<Button>(R.id.button_ingresar).setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+    private fun iniciarSesionConGoogle() {
+        lifecycleScope.launch {
+            try {
+                val googleIdOption = GetGoogleIdOption.Builder()
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId("103754154037-cohtjl0tq2bio32sd78rvml5ovsq76bo.apps.googleusercontent.com")
+                    .build()
+                val request = GetCredentialRequest.Builder()
+                    .addCredentialOption(googleIdOption)
+                    .build()
+                val credentialManager = CredentialManager.create(this@LoginActivity)
+                val result = credentialManager.getCredential(this@LoginActivity, request)
+                val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(result.credential.data)
+
+                SupabaseClient.client.auth.signInWith(IDToken){
+                    idToken = googleIdTokenCredential.idToken
+                    provider = Google
+                }
+                runOnUiThread {
+                    Toast.makeText(this@LoginActivity, "Inicio de sesión con Google exitoso", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finish()
+
+                }
+            }catch (e: Exception){
+                runOnUiThread {
+                    Toast.makeText(this@LoginActivity, "Error al iniciar sesión con Google: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
